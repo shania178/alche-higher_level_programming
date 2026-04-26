53b06160b2b83d64d6d3f8837338d4c4ce39ba61
root <b.tinkouree@alustudent.com>
2026-04-26 01:39:46 -0700

    Concatenating string

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton School!"
-str = str1 + " " + str2
-print(str)
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print(f"Welcome to {str1}!")
