cbbd213b79fe33d57f4b219780a0a9b7d0c9b099
root <b.tinkouree@alustudent.com>
2026-05-08 11:26:02 -0700

    square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size):
+        """Initialize a square with a given size."""
+        self.__size = size
