eae9e84d4558a9b3444406c242560c7bf3aeb02f
root <b.tinkouree@alustudent.com>
2026-05-09 09:49:06 -0700

    10. Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a Square class that inherits from Rectangle."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class."""
+
+    def __init__(self, size):
+        """Initialize square with validated size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Returns square description."""
+        return f"[Square] {self.__size}/{self.__size}"
