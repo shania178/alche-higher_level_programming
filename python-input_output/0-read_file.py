d17a50980a409871a4e4df9137eba73ef6da352b
root <b.tinkouree@alustudent.com>
2026-05-14 04:51:24 -0700

    readme.md file and reading file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""This module reads a UTF-8 file and prints its content to stdout."""
+
+
 def read_file(filename=""):
+    """Read a UTF-8 text file and print its content."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
