2841f9233badd08a0c75460b42d4371d3bd54e2c
root <b.tinkouree@alustudent.com>
2026-05-18 04:29:11 -0700

    Create user and granting privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,12 @@
--- lists all privileges of user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- create users and assign privileges
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- user_0d_1 = root user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- user_0d_2 = only SELECT + INSERT on user_2_db
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
