ae65ba1b4bbd6b9ac467afa9fc5878aea7709bd3
root <b.tinkouree@alustudent.com>
2026-05-18 04:37:59 -0700

    Privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,3 @@
--- user_0d_1 = root user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- user_0d_2 = only SELECT + INSERT on user_2_db
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
-FLUSH PRIVILEGES;
+-- Users don't exist
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
