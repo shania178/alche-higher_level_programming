1b6ac5458fa42e428f63e8e9754e2318f09fed5a
root <b.tinkouree@alustudent.com>
2026-05-23 15:59:21 -0700

    made changes again

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
+GRANT USAGE ON *.* TO 'user_0d_1'@'localhost';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
 REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, FIREWALL_EXEMPT, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
