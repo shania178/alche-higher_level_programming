9a6e427311abf89fab06012c63d5cc77bc89618f
shania178 <b.tinkouree@alustudent.com>
2026-06-03 17:08:44 +0000

    1. cURL to the end

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body only if the response status is 200
-curl -s -o /tmp/curl_body -w "%{http_code}" "$1" | grep -q "^200$" && cat /tmp/curl_body
+# Sends a GET request to a URL and displays the body only if the final response status is 200
+curl -sL "$1" -o /tmp/b -w "%{http_code}" | grep -q 200 && cat /tmp/b
