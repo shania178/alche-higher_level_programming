ae8b81ce97af62e5a5e8e9b9999f360d5e8db668
shania178 <b.tinkouree@alustudent.com>
2026-06-03 17:29:30 +0000

    3. cURL only methods

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends an OPTIONS request to a URL and displays the allowed HTTP methods
-curl -sI -X OPTIONS "$1" | grep -i "Allow:"
+curl -sI -X OPTIONS "$1" | grep -i "Allow: | sed 's/Allow: //'"
