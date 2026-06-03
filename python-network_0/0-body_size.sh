bf68854bb60104570a299e9a1e7a14ff22442032
shania178 <b.tinkouree@alustudent.com>
2026-06-03 16:49:47 +0000

    0. cURL body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL and displays the size of the response body
-curl -s "$1" -o /dev/null -w "%{size_download}"
+# Sends a request to a URL and displays the size of the response body in bytes
+curl -s -o /dev/null -w "%{size_download}\n" "$1"
