7e837a5f4fc0a0ac63534804368fd0f2c80ba3cb
shania178 <b.tinkouree@alustudent.com>
2026-06-06 19:41:42 +0000

    4. What's my status? #1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Fetches a URL using requests and displays the response body."""
+"""Fetches a URL and displays the body response using requests."""
 
 import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
 
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
