2abb56d880ba01d822cbeefee4084a27ece1c97a
shania178 <b.tinkouree@alustudent.com>
2026-06-06 18:56:20 +0000

    5. cURL POST parameters

---
+++ python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends a POST request with email and subject variables and displays the response body
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
