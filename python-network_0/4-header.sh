4145a22a46a57ae74404053ef1c43caa6272deb0
shania178 <b.tinkouree@alustudent.com>
2026-06-06 18:54:22 +0000

    4. cURL headers

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request with a custom header X-HolbertonSchool-User-Id: 98 and displays the response body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-School-User-Id: 98" "$1"
