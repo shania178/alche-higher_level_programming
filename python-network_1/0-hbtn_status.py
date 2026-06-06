eb702be212d89ec627d837a11bd7db4792fcbe55
shania178 <b.tinkouree@alustudent.com>
2026-06-06 18:59:46 +0000

    0. What's my status? #0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+pt that fetches a URL and displays the response body info using urllib"""
+import urllib.request
+
+with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
